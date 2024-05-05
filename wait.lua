1f6e8bea6c9ae70097ae3fe4baf17bd23a930a24
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-05-05 11:34:50 +0800

    更新 wait.lua

--- wait.lua
+++ wait.lua
@@ -1,5 +1,14 @@
 return {
-["名字"] = true,
-["名字"] = true,
-["名字"] = true,
+["zhaoxing66666"] = true,
+["wslzshhh"] = true,
+["646767T"] = true,
+["sijasoa"] = true,
+["Finaza520"] = true,
+["wsbbhhhhhhhhhh"] = true,
+["iwqqqqqqqqqs"] = true,
+["qwettt666"] = true,
+["cxcx981"] = 
+["sksjwj215"] = true,
+["Hudson_From4"] = true,
+true,
 }
