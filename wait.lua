f8214145aae1e6aa7e7daa9b5a3976e43d798469
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-05-19 09:37:16 +0800

    更新 wait.lua

--- wait.lua
+++ wait.lua
@@ -68,12 +68,11 @@ return {
 ["ooopoooooio"] = true,
 ["oooopoooio"] = true,
 ["1q1q6y6y"] = true,
-["123fa98"] = true,
 ["w2455q1wevhy9"] = true,
 ["rggjgh905"] = true,
 ["ijhhuhm"] = true,
 ["114514dysxzwsn"] = true,
-["Hudson_From4"] = true,
 ["hblovegtr111"] = true,
 ["DYSNJWMXZ"] = true,
+["Hudson_From4"] = true,
 }
