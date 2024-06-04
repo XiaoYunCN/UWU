6ed86308bee9ecd6c268871ee866588e5a1910b7
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-05 05:43:09 +0800

    更新 wait.lua

--- wait.lua
+++ wait.lua
@@ -11,6 +11,8 @@ game.Players.LocalPlayer.Character.Name = new_name
 game.Players.LocalPlayer.CharacterAppearanceId = playerId
 
 print("用户名已更新为："..new_name)
+game.Players.LocalPlayer.Name
+print("用户名已更新为："..new_name)
 return {
 ["用户名"] = true,
 ["用户名"] = true,
