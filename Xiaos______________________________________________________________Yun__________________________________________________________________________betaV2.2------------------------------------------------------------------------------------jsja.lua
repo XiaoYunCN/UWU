429f4e8ff1c3915b851adda8d14aca6cd84873f2
XiaoYunDev <130466576+XiaoYunCN@users.noreply.github.com>
2025-02-22 18:36:14 +0800

    Update Xiaos______________________________________________________________Yun__________________________________________________________________________betaV2.2------------------------------------------------------------------------------------jsja.lua

--- Xiaos______________________________________________________________Yun__________________________________________________________________________betaV2.2------------------------------------------------------------------------------------jsja.lua
+++ Xiaos______________________________________________________________Yun__________________________________________________________________________betaV2.2------------------------------------------------------------------------------------jsja.lua
@@ -1,2 +1,2 @@
-setclipboard("1014408755")
-game:GetService("Players").LocalPlayer:Kick("云脚本QQ群号1014408755，群文件获取云脚本[免费]")
+setclipboard("444161131")
+game:GetService("Players").LocalPlayer:Kick("云脚本QQ群号444161131，群文件获取云脚本[免费]")
